8dc0cfc4a8b9ef3e8f45960be9d4818b8703fb51
itskamilla <kamilla.aliakhmet@gmail.com>
2022-02-13 19:30:23 +0000

    Graph dfs was implemented iteratively

---
+++ graph/dfs-traversal.py
@@ -0,0 +1,19 @@
+graph = {'a': ['b','c'], 'b': ['d'], 'c': ['e'], 'd': ['f'], 'e': [],'f': []}
+
+# def dfsIterative(graph, source):
+#     stack = [source]
+#     while len(stack) > 0:
+#         current = stack.pop()
+#         print(current)
+#         for neighbor in graph[current]:
+#             stack.append(neighbor)
+      
+#dfsIterative(graph, 'a')
+
+def dfsRecursive(graph, source):
+    print(source)
+    for neighbor in graph(source):
+        dfsRecursive(graph, neighbor) 
+
+dfsRecursive(graph,'a')
+    
