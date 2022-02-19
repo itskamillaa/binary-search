f24b71f3df9b23a346d0a9a05ab64cfb358971e2
itskamilla <itskamillaa@users.noreply.github.com>
2022-02-19 11:30:17 +0000

    Connected components practice

--- graph/connected-components.py
+++ graph/connected-components.py
@@ -1,20 +1,22 @@
 def connectedComponents(graph):
     visited = set()
     count = 0
+
     for node in graph:
         if explore(graph, node, visited) == True:
-            count += 1
+            count+=1
     return count
 
-def explore(graph, current, visited):
-    if current in visited:
+def explore(graph, src, visited):
+    if src in visited:
         return False
-    visited.add(current)
-    print(visited)
-    for neighbor in graph[current]:
-        explore(graph, neighbor, visited)
-    
+    visited.add(src)
+
+    for neigbor in graph[src]:
+        explore(graph,neigbor,visited)
+
     return True
+
 
 
 graph ={1:[2],2:[1],3:[],4:[6],5:[6],6:[4,5,7,8],7:[6],8:[6]}
