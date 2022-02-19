519474866d7c0b9faad4118f4452be4f420599fe
itskamilla <itskamillaa@users.noreply.github.com>
2022-02-19 10:47:34 +0000

    hasPath - bfs solution

--- graph/hasPath.py
+++ graph/hasPath.py
@@ -1,17 +1,30 @@
 graph = {'f':['g','i'], 'g':['h'],'h':[],'i':['j','k'],'j':['i'],'k':[]}
 
-def hasPath(gpaph, src, dst):
+##DFS solution:
+# def hasPath(gpaph, src, dst):
 
-    #base case: when we reach the destination node
-    if src == dst:
-        return True
-    #check neighbors of source node
-    for neighbor in graph[src]:
-        #if there is a pth from neighbor of source node to the destination node, then there is a path from source to destination
-        if (graph,neighbor, dst):
-            return True
-    #if there is not path
+#     #base case: when we reach the destination node
+#     if src == dst:
+#         return True
+#     #check neighbors of source node
+#     for neighbor in graph[src]:
+#         #if there is a pth from neighbor of source node to the destination node, then there is a path from source to destination
+#         if (graph,neighbor, dst):
+#             return True
+#     #if there is not path
+#     return False
+
+##BFS solution:
+def hasPath(graph, src, dst):
+    queue = [src]
+    while len(queue)>0:
+        current = queue.pop(0)
+        #check if the current node we are on is the destination node
+        if current == dst:
+            return True 
+        for neighbor in graph[current]:
+            queue.append(neighbor)
     return False
 
-print(hasPath(graph, 'f','k'))
+print(hasPath(graph, 'h','j'))
 
